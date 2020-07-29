342f304e0109b759215b8e06b95db9156f6fb831
Tejeshwar <tejuamirthi@gmail.com>
2020-07-29 19:00:35 +0530

    Added image avator for business card

--- my_business_card/lib/main.dart
+++ my_business_card/lib/main.dart
@@ -9,7 +9,25 @@ class BusinessCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(),
+      home: Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text('My business card'),
+        ),
+        backgroundColor: Colors.teal,
+        body: SafeArea(
+          child: Center(
+            child: Column(
+              children: <Widget>[
+                CircleAvatar(
+                  backgroundImage: AssetImage("images/MyPic.jpg"),
+                  radius: 60.0,
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
